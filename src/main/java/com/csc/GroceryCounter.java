607466f2364b34ef2b96b3ee685c3e917f4fae43
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-09-14 22:11:13 -0500

    Update GroceryCounter.java

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -1,5 +1,21 @@
 package com.csc;
-
 public class GroceryCounter {
-  // Put your code here!
+  Integer[] counter = {0,0,0,0};
+  int overflow = 0;
+
+  public void tens() {
+    
+  }
+
+  public void ones() {
+
+  }
+
+  public void tenths() {
+
+  }
+  
+  public void hundreths(){
+
+  }
 }
