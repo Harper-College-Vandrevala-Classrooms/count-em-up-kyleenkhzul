34c8f104c647798392f26e6b80fe2f27f0bfe24c
kyleenkhzul <90794184+kyleenkhzul@users.noreply.github.com>
2024-09-15 17:57:06 -0500

    finished lab

--- src/main/java/com/csc/GroceryCounter.java
+++ src/main/java/com/csc/GroceryCounter.java
@@ -6,50 +6,58 @@ public class GroceryCounter {
   // This function increments the tens place everytime it is called
   public void tens() {
     if(digits[0] >= 10) {
-      overflow++;
       number_of_overflows();
       digits[0] = 0;
     }
     digits[0] += 1;
   }
 
-  // This function increments the ones place everytime it is called
+  // This function increments the ones place everytime it is called. If it exceeds 9, it adds 1 to the tens place. 
   public void ones() {
     if(digits[1] >= 10) {
-      overflow++;
-      number_of_overflows();
       digits[1] = 0;
+      digits[0] += 1;
     }
     digits[1] += 1;
+    number_of_overflows();
   }
 
-  // This function increments the tenths place everytime it is called
+  // This function increments the tenths place everytime it is called. If it exceeds 9, it adds 1 to the ones place. 
   public void tenths() {
     if(digits[2] >= 10) {
-      overflow++;
-      number_of_overflows();
       digits[2] = 0;
+      digits[1] += 1;
     }
     digits[2] += 1;
+    number_of_overflows();
   }
   
-  // This function increments the hundreths place everytime it is called
+  // This function increments the hundreths place everytime it is called. If it exceeds 9, it adds 1 to the tenths place. 
   public void hundreths(){
     if(digits[3] >= 10) {
-      overflow++;
-      number_of_overflows();
       digits[3] = 0;
+      digits[2] += 1;
     }
     digits[3] += 1;
+    number_of_overflows();
   }
 
   // This function neatly prints out the monetary total
-  public void total() {
-    System.out.println("The total amount is $" + digits[0] + digits[1] + "." + digits[2] + digits[3]);
+  public String total() {
+    if(digits[0] == 0) {
+      return ("The total amount is $" + digits[1] + "." + digits[2] + digits[3]);
+    }
+    else {
+    return ("The total amount is $" + digits[0] + digits[1] + "." + digits[2] + digits[3]);
+    }
   }
 
   // This function returns the number of overflows
   public int number_of_overflows() {
+    if(digits[0] >= 10) {
+      digits[0] %= 10;
+      overflow++;
+    }
     return overflow;
   }
 
--- src/test/java/com/csc/TestGroceryCounter.java
+++ src/test/java/com/csc/TestGroceryCounter.java
@@ -14,11 +14,72 @@ public class TestGroceryCounter {
     counter = new GroceryCounter();
   }
 
-  // Put your tests here!
-
   @Test
-  void itWorks() {
-    assertEquals(true, true);
+  void itWorksForANewCounterGroceryCounterObject() {
+    assertEquals(counter.total(), "The total amount is $0.00");
   }
 
+  @Test
+  void itWorksForCallingHundrethsFunction() {
+    counter.hundreths();
+    assertEquals(counter.total(), "The total amount is $0.01");
+  }
+
+  @Test
+  void itWorksForCallingTenthsFunction() {
+    counter.tenths();
+    assertEquals(counter.total(), "The total amount is $0.10");
+  }
+
+  @Test
+  void itWorksForCallingOnesFunction() {
+    counter.ones();
+    assertEquals(counter.total(), "The total amount is $1.00");
+  }
+
+  @Test
+  void itWorksForCallingTensFunction() {
+    counter.tens();
+    assertEquals(counter.total(), "The total amount is $10.00");
+  }
+
+  @Test
+  void itWorksForTwentyDollarsAndOneCent() {
+    counter.tens();
+    counter.tens();
+    counter.hundreths();
+    assertEquals(counter.total(), "The total amount is $20.01");
+  }
+
+  @Test 
+  void itWorksForOverflowing() {
+    for(int i = 0; i < 10; i++) {
+      counter.tens();
+    }
+    assertEquals(counter.number_of_overflows(), 1);
+  }
+
+  @Test
+  void itWorksForTwoOverFlowingAndTwentyDollarsAndFiftyCents() {
+    for(int i = 0; i < 5; i++) {
+      counter.tenths();
+    }
+    counter.tens();
+    counter.tens();
+    for(int i = 0; i < 20; i++) {
+      counter.tens();
+    }
+    assertEquals(counter.number_of_overflows(), 2);
+    assertEquals(counter.total(), "The total amount is $20.50");
+  }
+
+  @Test
+  void itWorksForClearing() {
+    counter.ones();
+    counter.tens();
+    counter.hundreths();
+    counter.tenths();
+    counter.clear();
+    assertEquals(counter.total(), "The total amount is $0.00");
+  }
 }
